492ff05e1159f892b3d2d3aa20d302b0c71bef42
rice-guyenn <er879457@dal.ca>
2017-03-02 13:00:02 -0400

    Added domainCheck method

--- a3/src/main/java/a3/a3/validAddress.java
+++ a3/src/main/java/a3/a3/validAddress.java
@@ -12,4 +12,28 @@ public class validAddress {
 		}
 		return check;
 	}
+	
+	public boolean domainCheck(String in) {
+		boolean check = true;
+		String dom = "";
+		for (int i = in.length()-3; i < in.length(); i++) {
+			char c = in.charAt(i);
+			dom = dom + String.valueOf(c);
+		}
+		if (dom.charAt(0)!='.') {
+			check = false;
+		} else {
+			switch(dom.length()) {
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			default: check = false;
+				break;
+			}
+		}
+		return check;
+	}
 }
